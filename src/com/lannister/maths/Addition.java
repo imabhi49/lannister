2db1e087c2ad6c4a0d6fc825543dd0dacececea3
imabhi49 <abhishekkamble4990@gmail.com>
2022-11-19 20:14:13 +0530

    Initial mathematics commit

---
+++ src/com/lannister/maths/Addition.java
@@ -0,0 +1,10 @@
+package com.lannister.maths;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = a + b;
+        System.out.println(a+=b);
+    }
+}
